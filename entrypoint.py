40a19a4d2a892613b677d9ab79dda63a6c2c2fa9
Alex Herman <aherman@insight2profit.com>
2020-06-30 10:50:44 -0400

    Enables pagerduty api call

--- entrypoint.py
+++ entrypoint.py
@@ -13,10 +13,10 @@ def main(routing_key, dedup_key, summary, source, custom_details=None):
     if custom_details is not None:
         details = json.loads(custom_details)
 
-    print(f"{routing_key}-{dedup_key}: {summary} {source} {custom_details}")
+    print(f"{dedup_key}: {summary} {source} {custom_details}")
 
-    #session = pdpyras.EventsAPISession(routing_key)
-    #session.trigger(summary, source, dedup_key=dedup_key, custom_details=details)
+    session = pdpyras.EventsAPISession(routing_key)
+    session.trigger(summary, source, dedup_key=dedup_key, custom_details=details)
 
 if __name__ == '__main__':
     main()
