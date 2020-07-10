fa5ab2a1501cee9a0ff2473633d7b9efbddc90d3
Alex Herman <aherman@insight2profit.com>
2020-07-10 10:37:19 -0400

    Adds links to event arguments

--- entrypoint.py
+++ entrypoint.py
@@ -8,15 +8,18 @@ import json
 @click.argument("summary")
 @click.argument("source")
 @click.argument("custom_details", required=False)
-def main(routing_key, dedup_key, summary, source, custom_details=None):
-    print("Staring event API call")
+@click.argument("links", required=False)
+def main(routing_key, dedup_key, summary, source, custom_details=None, links=None):
     if custom_details is not None:
         details = json.loads(custom_details)
 
-    print(f"{dedup_key}: {summary} {source} {custom_details}")
+    if links is not None:
+        event_links = json.loads(links)
+
+    print(f"{dedup_key}: {summary} {source} {custom_details} {event_links}")
 
     session = pdpyras.EventsAPISession(routing_key)
-    session.trigger(summary, source, dedup_key=dedup_key, custom_details=details)
+    session.trigger(summary, source, dedup_key=dedup_key, custom_details=details, links=event_links)
 
 if __name__ == '__main__':
     main()
